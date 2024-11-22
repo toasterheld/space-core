450d79c887a751a977a57e5dd004d7b87abbe0ed
TheAnimalDrummer <Luigi.schotten@outlook.de>
2024-11-22 20:17:22 +0100

    create Entity and Component Systems

--- space-core/src/main/java/engine/component/Component.java
+++ space-core/src/main/java/engine/component/Component.java
@@ -1,4 +1,26 @@
 package engine.component;
 
+import engine.entity.Entity;
+import engine.util.Vector2D;
+
 public class Component {
+
+    private Vector2D pos;
+    private Vector2D offset;
+
+    public Component(Entity parent){
+        pos = parent.getPos().add(offset);
+    }
+
+    public void init(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void render(){
+
+    }
 }
--- space-core/src/main/java/engine/entity/Entity.java
+++ space-core/src/main/java/engine/entity/Entity.java
@@ -1,4 +1,38 @@
 package engine.entity;
 
+import engine.component.Component;
+import engine.util.Vector2D;
+
+import java.util.ArrayList;
+
 public class Entity {
+    protected Vector2D pos;
+    protected ArrayList<Component> components;
+
+
+
+    protected Component addComponent(Component component){
+        components.add(component);
+        return component;
+    }
+
+
+    public void init(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void render(){
+        for(int i = 0; i < components.size(); i++){
+            components.get(i).render();
+        }
+    }
+
+
+    public Vector2D getPos() {
+        return pos;
+    }
 }
